890a92d6c1d59ec14e6547c75d68c441b43b2f2a
Jonathan Lian <jonathan.hahaha@gmail.com>
2017-04-02 06:48:34 -0400

    another session bugfix and added logout buttons

--- routes/signIn.js
+++ routes/signIn.js
@@ -17,52 +17,45 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
     var email = req.body.email;
     var password = req.body.password;
-    firebase.auth().onAuthStateChanged(function (user) {
-        console.log(firebase.auth());
-        if (!user) {
-            firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
-                console.log(firebase.auth());
-                firebase.auth().currentUser.getToken(true).then(function (idToken) {
-                    admin.auth().verifyIdToken(idToken)
-                        .then(function (decodedToken) {
-                            var uid = decodedToken.uid;
-                            models.User.findOne({
-                                where: { firebase_id: uid }
-                            }).then(function (user) {
-                                console.log(user);
-                                if (user && user.isInitial == true) { // if user w/ correct pass exists
-                                    res.redirect('/createProfile');
-                                } else if (!user) { // user doesn't exist
-                                    res.redirect('/signIn');
+        firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
+            console.log(firebase.auth());
+            firebase.auth().currentUser.getToken(true).then(function (idToken) {
+                admin.auth().verifyIdToken(idToken)
+                    .then(function (decodedToken) {
+                        var uid = decodedToken.uid;
+                        models.User.findOne({
+                            where: { firebase_id: uid }
+                        }).then(function (user) {
+                            console.log(user);
+                            if (user && user.isInitial == true) { // if user w/ correct pass exists
+                                res.redirect('/createProfile');
+                            } else if (!user) { // user doesn't exist
+                                res.redirect('/signIn');
+                            } else {
+                                // if worker
+                                if (user.role == 1) { // worker
+                                    res.redirect('/workerPortal');
+                                } else if (user.role == 2) { // customer
+                                    res.redirect('/customerPortal');
+                                } else if (user.role == 3) { // security
+                                    console.log('securityPeron');
                                 } else {
-                                    // if worker
-                                    if (user.role == 1) { // worker
-                                        res.redirect('/workerPortal');
-                                    } else if (user.role == 2) { // customer
-                                        res.redirect('/customerPortal');
-                                    } else if (user.role == 3) { // security
-                                        console.log('securityPeron');
-                                    } else {
-                                        console.log("error!");
-                                    }
+                                    console.log("error!");
                                 }
-                            });
-                        }).catch(function (error) {
-                        // Handle error
-                    });
-                }).catch(function (error) {
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
+                            }
+                        });
+                    }).catch(function (error) {
+                    // Handle error
                 });
             }).catch(function (error) {
                 var errorCode = error.code;
                 var errorMessage = error.message;
-                res.render('signIn', {title: 'Sign In'});
             });
-        } else {
-            console.log('error');
-        }
-    });
+        }).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            res.render('signIn', {title: 'Sign In'});
+        });
 });
 
 module.exports = router;
