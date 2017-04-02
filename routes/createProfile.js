46e6368f5b8181dc824c937bee6bbf0f1ee32e50
Jonathan Lian <jonathan.hahaha@gmail.com>
2017-04-02 10:16:50 -0400

    lmao

--- routes/createProfile.js
+++ routes/createProfile.js
@@ -12,45 +12,38 @@ router.post('/', function(req, res) {
     var firstname = req.body.firstname;
     var lastname = req.body.lastname;
     var role = req.body.role;
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            // User is signed in.
-            firebase.auth().currentUser.getToken(true).then(function(idToken) {
-                admin.auth().verifyIdToken(idToken)
-                    .then(function (decodedToken) {
-                        var uid = decodedToken.uid;
-                        models.User.findOne({ where: {
-                            firebase_id: uid
-                        }}).then(function(user) {
-                            if (user) {
-                                user.updateAttributes({
-                                    firstname: firstname,
-                                    lastname: lastname,
-                                    isInitial: false,
-                                    role: role
-                                });
-                                if (user.role == 1) { // if worker
-                                    res.redirect('/workerPortal');
-                                } else if (user.role == 2) { // if customer
-                                    res.redirect('/customerPortal');
-                                } else if (user.role == 3) { // if securityPerson
-                                    console.log('securityPerson');
-                                } else {
-                                    console.log("error!");
-                                }
-                            } else {
-                                console.log('user doesn\'t exist!');
-                            }
-                        }).catch(function(error) {
-                            var errorCode = error.code;
-                            var errorMessage = error.message;
+    firebase.auth().currentUser.getToken(true).then(function(idToken) {
+        admin.auth().verifyIdToken(idToken)
+            .then(function (decodedToken) {
+                var uid = decodedToken.uid;
+                models.User.findOne({ where: {
+                    firebase_id: uid
+                }}).then(function(user) {
+                    if (user) {
+                        user.updateAttributes({
+                            firstname: firstname,
+                            lastname: lastname,
+                            isInitial: false,
+                            role: role
                         });
-                    });
+                        console.log('asdiuhsaiudhs', user);
+                        if (user.role == 1) { // if worker
+                            res.redirect('/workerPortal');
+                        } else if (user.role == 2) { // if customer
+                            res.redirect('/customerPortal');
+                        } else if (user.role == 3) { // if securityPerson
+                            console.log('securityPerson');
+                        } else {
+                            console.log("error!");
+                        }
+                    } else {
+                        console.log('user doesn\'t exist!');
+                    }
+                }).catch(function(error) {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                });
             });
-        } else {
-            // No user is signed in.
-            console.log('No user!');
-        }
     });
 });
 
