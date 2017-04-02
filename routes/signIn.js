e66871b6ef8a79c5827d12198607dd57a6e9eb8b
Jonathan Lian <jonathan.hahaha@gmail.com>
2017-04-01 22:20:43 -0400

    more bugfix with sign-in

--- routes/signIn.js
+++ routes/signIn.js
@@ -17,41 +17,51 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
     var email = req.body.email;
     var password = req.body.password;
-    firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
-        firebase.auth().currentUser.getToken(true).then(function (idToken) {
-            admin.auth().verifyIdToken(idToken)
-                .then(function (decodedToken) {
-                    var uid = decodedToken.uid;
-                    models.User.findOne({
-                        where: { firebase_id: uid }
-                    }).then(function (user) {
-                        if (user && user.isInitial == true) { // if user w/ correct pass exists
-                            res.redirect('/createProfile');
-                        } else if (!user) { // user doesn't exist
-                            res.redirect('/signIn');
-                        } else { // if isInitial is false
-                            if (user.role == 1) {
-                                console.log('worker');
-                            } else if (user.role == 2) { // if customer
-                                res.redirect('/customerPortal');
-                            } else if (user.role == 3) {
-                                console.log('securityPeron');
-                            } else {
-                                console.log("error!");
-                            }
-                        }
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            console.log('signed in!');
+        } else {
+            // No user is signed in.
+            firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
+                firebase.auth().currentUser.getToken(true).then(function (idToken) {
+                    admin.auth().verifyIdToken(idToken)
+                        .then(function (decodedToken) {
+                            var uid = decodedToken.uid;
+                            models.User.findOne({
+                                where: { firebase_id: uid }
+                            }).then(function (user) {
+                                console.log(user);
+                                if (user && user.isInitial == true) { // if user w/ correct pass exists
+                                    res.redirect('/createProfile');
+                                } else if (!user) { // user doesn't exist
+                                    res.redirect('/signIn');
+                                } else {
+                                    // if worker
+                                    if (user.role == 1) {
+                                        console.log('worker');
+                                    } else if (user.role == 2) {
+                                        res.redirect('/customerPortal');
+                                    } else if (user.role == 3) {
+                                        console.log('securityPeron');
+                                    } else {
+                                        console.log("error!");
+                                    }
+                                }
+                            });
+                        }).catch(function (error) {
+                        // Handle error
                     });
                 }).catch(function (error) {
-                // Handle error
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                });
+            }).catch(function (error) {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                res.render('signIn', {title: 'Sign In'});
             });
-        }).catch(function (error) {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-        });
-    }).catch(function (error) {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        res.render('signIn', {title: 'Sign In'});
+        }
     });
 });
 
